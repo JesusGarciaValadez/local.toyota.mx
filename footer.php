bca445a2d8e422bfb3070b28020ca1cb34c3232e
Oskar <bodom_24_7@hotmail.com>
2014-04-02 12:20:28 -0600

    links

--- footer.php
+++ footer.php
@@ -1,19 +1,19 @@
 	<footer class="footer container">
 		<div class="redes">
-			<a href=""><i class="icon-twitter"></i><span>@Toyota_Mx</span></a>
-			<a href=""><i class="icon-instagram"></i><span>/ToyotaMex</span></a>
-			<a href=""><i class="icon-facebook"></i><span>/ToyotaMex</span></a>
-			<a href=""><i class="icon-youtube-play"></i><span></span></a>
+			<a href="https://twitter.com/ToyotaMex" target="_blank"><i class="icon-twitter"></i><span>@Toyota_Mx</span></a>
+			<a href="http://instagram.com/ToyotaMex" target="_blank"><i class="icon-instagram"></i><span>/ToyotaMex</span></a>
+			<a href="https://www.facebook.com/ToyotaMex" target="_blank"><i class="icon-facebook"></i><span>/ToyotaMex</span></a>
+			<a href="https://www.youtube.com/user/ToyotaMex" target="_blank"><i class="icon-youtube-play"></i><span></span></a>
 		</div>
 		<div class="telefonos">
-			<a href="">toyota.com.mx</a>
+			<a href="http://www.toyota.com.mx/" target="_blank">toyota.com.mx</a>
 			<img src="images/telefonos.png">
 		</div>
 		<div class="derechos">Todos los Derechos Reservados toyota® 2014</div>
 		<div class="terminos">
-			<a href="">Términos y condiciones</a>
-			<a href="">Legales</a>
-			<a href="">Aviso de privacidad</a>
+			<a href="http://toyota.com.mx/contenidos/terminos-legales-y-condiciones/" target="_blank">Términos y condiciones</a>
+			<a href="http://toyota.com.mx/contenidos/terminos-legales-y-condiciones/" target="_blank">Legales</a>
+			<a href="http://toyota.com.mx/contenidos/aviso-de-privacidad/" target="_blank">Aviso de privacidad</a>
 		</div>
 	</footer>
 	<script src="js/core.js"></script>
