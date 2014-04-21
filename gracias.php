d1f47be011b2db35364fca8f30ab064ea3b0ad7e
Oskar <bodom_24_7@hotmail.com>
2014-04-21 13:07:37 -0500

    Pagina Gracias

---
+++ gracias.php
@@ -0,0 +1,15 @@
+<?php include('header.php'); ?>
+
+	<section class="secPrueba container">
+		<article>
+			<div class="textGracias sixteen columns">
+				<h2>¡Gracias por registrarte!</h2>
+                <div>
+                    <p>Muy pronto nos comunicaremos contigo para que<br>vivas la nueva actitud Highlander.</p>
+                </div>
+                <a href="prueba.php">Regresar</a>
+			</div>
+		</article>
+	</section>
+
+<?php include('footer.php'); ?>
