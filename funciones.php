589a683bb636bc583efca0efae1c67f108236b89
pimediana21 <lunavemultimedios@lunaves-iMac.local>
2014-04-29 10:45:51 -0500

    actualización highlander
    actualización de higlander  base

---
+++ funciones.php
@@ -0,0 +1,72 @@
+<? include('conexion/conexion.php'); ?>
+<?php
+function conectaBaseDatos(){
+	try{
+		$conexion = new PDO("mysql:host=$hostname;dbname=$database",
+							$username,
+							$password,
+							array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+		
+		$conexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+		return $conexion;
+	}
+	catch (PDOException $e){
+		die ("No se puede conectar a la base de datos". $e->getMessage());
+	}
+}
+
+function dameEstado(){
+	$resultado = false;
+	$consulta = "SELECT * FROM dealers";
+	
+	$conexion = conectaBaseDatos();
+	$sentencia = $conexion->prepare($consulta);
+	
+	try {
+		if(!$sentencia->execute()){
+			print_r($sentencia->errorInfo());
+		}
+		$resultado = $sentencia->fetchAll();
+		//$resultado = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+		$sentencia->closeCursor();
+	}
+	catch(PDOException $e){
+		echo "Error al ejecutar la sentencia: \n";
+			print_r($e->getMessage());
+	}
+	
+	return $resultado;
+}
+
+function dameSucursal($estado = ''){
+	$resultado = false;
+	$consulta = "SELECT * FROM dealers";
+	
+	if($estado != ''){
+		$consulta .= " WHERE id = :estado";
+	}
+	
+	$conexion = conectaBaseDatos();
+	$sentencia = $conexion->prepare($consulta);
+	$sentencia->bindParam('estado',$estado);
+	
+	try {
+		if(!$sentencia->execute()){
+			print_r($sentencia->errorInfo());
+		}
+		$resultado = $sentencia->fetchAll();
+		//$resultado = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+		$sentencia->closeCursor();
+	}
+	catch(PDOException $e){
+		echo "Error al ejecutar la sentencia: \n";
+			print_r($e->getMessage());
+	}
+	
+	return $resultado;
+}
+
+
+
+
+?>
--- prueba.php
+++ prueba.php
@@ -1,42 +1,116 @@
+<?php
+require_once("funciones.php");
+?>
 <?php include('header.php'); ?>
-
-	<section class="secPrueba container">
-		<article>
-			<div class="textPrueba sixteen columns">
-				<h2>Prueba de manejo</h2>
-				<p>Vive la mejor experiencia de manejo con la nueva generación Highlander 14.</p>
-			</div>
-			<div class="contPrueba sixteen columns">
-				<p>Por favor ingresa tus datos  y selecciona tu sucursal más cercana.</p>
-				<form action="" method="get">
-                	<input type="text" placeholder="*NOMBRE" required="true">
-                    <input type="text" placeholder="*CORREO ELECTRÓNICO" required="true">
-                    <input type="text" placeholder="*NÚMERO TELEFÓNICO (OPCIONAL)">
+    <section class="secPrueba container">
+        <article>
+            <div class="textPrueba sixteen columns">
+                <h2>Prueba de manejo</h2>
+                <p>Vive la mejor experiencia de manejo con la nueva generación Highlander 14.</p>
+            </div>
+            <div class="contPrueba sixteen columns">
+                <p>Por favor ingresa tus datos  y selecciona tu sucursal más cercana.</p>
+                <form action="" method="get" enctype="multipart/form-data">
+                    <input type="text" name="nombre" id="nombre" placeholder="*NOMBRE" required="true">
+                    <input type="text" name="correo" id="correo" placeholder="*CORREO ELECTRÓNICO" required="true">
+                    <input type="text" name="telefono" id="telefono" placeholder="*NÚMERO TELEFÓNICO (OPCIONAL)">
                     <label>SELECCIONA SUCURSAL</label>
                     <div class="selectBox">
                         <div class="box" id="box">Estado</div>
-                        <select name="mySelectBox" id="mySelectBox" onchange="this.parentNode.getElementsByTagName('div')[0].innerHTML=this.options[this.selectedIndex].text">
-                            <option value="1" selected="selected" class="primero">Estado</option>
-                            <option value="2">Escribir un libro</option>
-                            <option value="3">Ser Padre</option>
-                            <option value="4">Hacer un Blog</option>
-                            <option value="5" class="ultimo">Un texto largo para ver que efecto produce</option>
-                        </select>
-                    </div>
+                            <select name="estado" id="estado" onchange="this.parentNode.getElementsByTagName('div')[0].innerHTML=this.options[this.selectedIndex].text" required>
+                                            <option value="" selected="selected" class="primero">- Seleccione un Estado -</option>
+                                    <?php
+                                    $estados = dameEstado();
+                                    foreach($estados as $indice => $registro){
+                                        echo "<option value=".$registro['id'].">".$registro['estado']."</option>";
+                                    }
+                                    ?>
+                             </select>
+                        </div>
                     <div class="selectBox">
-                        <div class="box" id="box">Sucursal</div>
-                        <select name="mySelectBox" id="mySelectBox" onchange="this.parentNode.getElementsByTagName('div')[0].innerHTML=this.options[this.selectedIndex].text">
-                            <option value="1" selected="selected" class="primero">Sucursal</option>
-                            <option value="2">Escribir un libro</option>
-                            <option value="3">Ser Padre</option>
-                            <option value="4">Hacer un Blog</option>
-                            <option value="5" class="ultimo">Un texto largo para ver que efecto produce</option>
-                        </select>
+                    <div class="box" id="box">Sucursal</div>
+                                <select name="sucursal" id="sucursal" required>
+                                            <option value="">- primero seleccion un estado -</option>
+                                </select>
                     </div>
-                    <input type="submit" value="enviar">
+                    <input type="submit" value="Enviar" name="enviar" id="enviar">
                 </form>
-			</div>
-		</article>
-	</section>
+            </div>
+        </article>
+    </section>
+
+<script>
+$("#estado").on("change", buscarSucursales);
+function buscarSucursales(){
+    //$("#localidad").html("<option value=''>- primero seleccione un municipio -</option>");
+    
+    $estado = $("#estado").val();
+    
+    if($estado == ""){
+            $("#sucursal").html("<option value=''>- primero seleccione un estado -</option>");
+    }
+    else {
+        $.ajax({
+            dataType: "json",
+            data: {"estado": $estado},
+            url:   'buscar.php',
+            type:  'post',
+            beforeSend: function(){
+                //Lo que se hace antes de enviar el formulario
+                },
+            success: function(respuesta){
+                //lo que se si el destino devuelve algo
+                $("#sucursal").html(respuesta.html);
+            },
+            error:  function(xhr,err){ 
+                alert("readyState: "+xhr.readyState+"\nstatus: "+xhr.status+"\n \n responseText: "+xhr.responseText);
+            }
+        });
+    }
+}
+</script>
+<? 
+ extract($_POST);
+    extract($_GET);
+if($nombre!="" && $correo!="" && $estado!="" && $sucursal!="")
+{
+    mysql_select_db($database, $conexion); 
+
+$query_datos="SELECT * FROM dealers WHERE id='$estado'";
+$datos=mysql_query($query_datos, $conexion) or die (mysql_error());
+$row_datos=mysql_fetch_assoc($datos);
+$totalRows_datos=mysql_num_rows($datos);
+//$correo_db=row_datos['email']; 
+
+mysql_select_db($database, $conexion);
+    $sql = "insert into correos (nombre, correo, telefono, estado, sucursal) values ('$nombre','$correo','$telefono','$estado','$sucursal')";
+    //echo $sql;    
+    mysql_query($sql, $conexion) or die(mysql_error());
+
+    $asunto="solicitud";
+    $formato="Content-type: text/html\n";
+    
+          
+    $formato.= "From: adriana@lunave.com <adriana@lunave.com>\r\n"; 
+    $para=$row_datos['email'];
+    $para2='adriana@lunave.com';
+
+    
+    $sucursal1=$row_datos['nombre'];
+    $estado1=$row_datos['estado'];
+
+    $mensaje="Contacto
+        Nombre:   $nombre
+        Correo:   $correo
+        Teléfono: $telefono
+        Estado:   $estado1
+        Sucursal: $sucursal1
+       ";
+    mail($para2,$asunto,$mensaje,$formato);
+}else{
+   
+}
+   
+?>
 
 <?php include('footer.php'); ?>
