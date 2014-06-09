ff1516406ff89836dcc4d69bb24f4cf233b5719b
root <root@ip-172-31-1-253.us-west-2.compute.internal>
2014-06-09 18:31:06 +0000

    update analytics + title

--- index.php
+++ index.php
@@ -783,14 +783,16 @@ $browser = new Browser();
     })(window,document,'script','dataLayer','GTM-MC8TPH');</script>
     <!-- End Google Tag Manager -->
 
-    <script>
-     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-     ga('create', 'UA-43443825-1', 'corollaexperience.com');
-     ga('send', 'pageview');
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-49566515-1', 'toyotahighlander.com.mx');
+    ga('send', 'pageview');
+
     </script>
     <script type="text/javascript">
     var fb_param = {};
