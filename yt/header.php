441e9f0d20dca75486c6b87ff5612fc3ecf2d8be
elionaz <elionaz@me.com>
2014-06-10 16:56:08 -0500

    update facebook code

--- yt/header.php
+++ yt/header.php
@@ -39,6 +39,23 @@ document.write('<scr'+'ipt src="HTTPS://bs.serving-sys.com/Serving/ActivityServe
 <img width="1" height="1" style="border:0" src="HTTPS://bs.serving-sys.com/Serving/ActivityServer.bs?cn=as&amp;ActivityID=489035&amp;ns=1"/>
 </noscript>
 
+<!--FB Code Stuff -->
+<script>(function() {
+  var _fbq = window._fbq || (window._fbq = []);
+  if (!_fbq.loaded) {
+          var fbds = document.createElement('script');
+              fbds.async = true;
+              fbds.src = '//connect.facebook.net/en_US/fbds.js';
+                  var s = document.getElementsByTagName('script')[0];
+                  s.parentNode.insertBefore(fbds, s);
+                      _fbq.loaded = true;
+                    }
+    _fbq.push(['addPixelId', '306593512832230']);
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', 'PixelInitialized', {}]);
+</script>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=306593512832230&amp;ev=NoScript" /></noscript>
 </head>
 
 <body>
