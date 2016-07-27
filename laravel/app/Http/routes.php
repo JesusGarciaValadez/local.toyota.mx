df728c64e994ecf40b8d2e76b08a2a36d30c7bd2
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-07-27 20:00:33 +0000

    Create routes for admin

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -15,30 +15,23 @@ Route::get( '/', [ 'as' => 'home', 'uses' => 'HomeController@index' ] );
 
 Route::get( '/home', [ 'as' => 'home', 'uses' => 'HomeController@index' ] );
 
+
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Routes File
 |--------------------------------------------------------------------------
 |
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
 |
 */
-
-Route::group( [ 'middleware' => [ 'web' ] ], function ()
+Route::group( [ 'prefix' => 'admin' ], function ( )
 {
-  /*
-  |--------------------------------------------------------------------------
-  | Routes File
-  |--------------------------------------------------------------------------
-  |
-  | Here is where you will register all of the routes in an application.
-  | It's a breeze. Simply tell Laravel the URIs it should respond to
-  | and give it the controller to call when that URI is requested.
-  |
-  */
-  Route::get( '/admin', [ 'as' => 'admin', 'uses' => 'Admin\EditController@index' ] );
-
-  Route::auth();
+  Route::get( '', [ 'as' => 'index', 'uses' => 'Admin\AdminController@index' ] );
+  Route::get( 'editar/{id}', [ 'as' => 'edit', 'uses' => 'Admin\AdminController@edit' ] );
+  Route::put( 'actualizar/{id}', [ 'as' => 'update', 'uses' => 'Admin\AdminController@update' ] );
+  Route::delete( 'borrar/{id}', [ 'as' => 'destroy', 'uses' => 'Admin\AdminController@destroy' ] );
 } );
+
+Route::auth( );
