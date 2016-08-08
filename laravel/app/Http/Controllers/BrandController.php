039d55dcc67cc5f51108db2da6a968c996f6d725
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-08-08 18:47:34 +0000

    Better route handling

--- laravel/app/Http/Controllers/BrandController.php
+++ laravel/app/Http/Controllers/BrandController.php
@@ -10,79 +10,81 @@ use Highlander\Http\Requests\BrandUpdatedRequest;
 
 class BrandController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    //
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    //
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    $brands = \Highlander\Brands::all( );
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+    return view( 'admin.index' )->withBrands( $brands );
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    //
+  }
 }
--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -32,72 +32,21 @@ Route::group( [ 'prefix' => 'admin', 'middleware' => 'auth' ], function ( )
   Route::get( '{id}/editar', [ 'as' => 'edit', 'uses' => 'Admin\AdminController@edit' ] );
   Route::delete( '{id}/borrar', [ 'as' => 'destroy', 'uses' => 'Admin\AdminController@destroy' ] );
 
-  Route::group( [ 'prefix' => '{id}' ], function ( ) {
-    Route::group( [ 'prefix' => 'brand' ], function ( ) {
-      Route::get( 'editar', [ 'user' => 'BrandController@edit' ] );
-      Route::put( 'actualiza', [ 'user' => 'BrandController@update' ] );
-      Route::delete( 'borrar', [ 'user' => 'BrandController@delete' ] );
-    } );
-    Route::group( [ 'prefix' => 'title_h1' ], function ( ) {
-      Route::get( 'editar', [ 'user' => 'TitleH1Controller@edit' ] );
-      Route::put( 'actualiza', [ 'user' => 'TitleH1Controller@update' ] );
-      Route::delete( 'borrar', [ 'user' => 'TitleH1Controller@delete' ] );
-    } );
-    Route::group( [ 'prefix' => 'slider_features_id' ], function ( ) {
-      Route::get( 'editar', [ 'user' => 'SliderFeaturesIDController@edit' ] );
-      Route::put( 'actualiza', [ 'user' => 'SliderFeaturesIDController@update' ] );
-      Route::delete( 'borrar', [ 'user' => 'SliderFeaturesIDController@delete' ] );
-    } );
-    Route::group( [ 'prefix' => 'title_slider_features_id' ], function ( ) {
-      Route::get( 'editar', [ 'user' => 'TitleSliderFeaturesIDController@edit' ] );
-      Route::put( 'actualiza', [ 'user' => 'TitleSliderFeaturesIDController@update' ] );
-      Route::delete( 'borrar', [ 'user' => 'TitleSliderFeaturesIDController@delete' ] );
-    } );
-    Route::group( [ 'prefix' => 'title_gallery_fancybox' ], function ( ) {
-      Route::get( 'editar', [ 'user' => 'TitleGalleryFancyboxController@edit' ] );
-      Route::put( 'actualiza', [ 'user' => 'TitleGalleryFancyboxController@update' ] );
-      Route::delete( 'borrar', [ 'user' => 'TitleGalleryFancyboxController@delete' ] );
-    } );
-    Route::group( [ 'prefix' => 'gallery_fancyboxes_id' ], function ( ) {
-      Route::get( 'editar', [ 'user' => 'GalleryFancyboxesController@edit' ] );
-      Route::put( 'actualiza', [ 'user' => 'GalleryFancyboxesController@update' ] );
-      Route::delete( 'borrar', [ 'user' => 'GalleryFancyboxesController@delete' ] );
-    } );
-    Route::group( [ 'prefix' => 'description_gallery_fancybox' ], function ( ) {
-      Route::get( 'editar', [ 'user' => 'DescriptionGalleryFancyboxController@edit' ] );
-      Route::put( 'actualiza', [ 'user' => 'DescriptionGalleryFancyboxController@update' ] );
-      Route::delete( 'borrar', [ 'user' => 'DescriptionGalleryFancyboxController@delete' ] );
-    } );
-    Route::group( [ 'prefix' => 'title_versions_gallery' ], function ( ) {
-      Route::get( 'editar', [ 'user' => 'TitleVersionsController@edit' ] );
-      Route::put( 'actualiza', [ 'user' => 'TitleVersionsController@update' ] );
-      Route::delete( 'borrar', [ 'user' => 'TitleVersionsController@delete' ] );
-    } );
-    Route::group( [ 'prefix' => 'car_description_gallery_one_id' ], function ( ) {
-      Route::get( 'editar', [ 'user' => 'CarDescriptionGalleryOneIDController@edit' ] );
-      Route::put( 'actualiza', [ 'user' => 'CarDescriptionGalleryOneIDController@update' ] );
-      Route::delete( 'borrar', [ 'user' => 'CarDescriptionGalleryOneIDController@delete' ] );
-    } );
-    Route::group( [ 'prefix' => 'car_description_gallery_two_id' ], function ( ) {
-      Route::get( 'editar', [ 'user' => 'CarDescriptionGalleryTwoIDController@edit' ] );
-      Route::put( 'actualiza', [ 'user' => 'CarDescriptionGalleryTwoIDController@update' ] );
-      Route::delete( 'borrar', [ 'user' => 'CarDescriptionGalleryTwoIDController@delete' ] );
-    } );
-    Route::group( [ 'prefix' => 'title_driving_animation' ], function ( ) {
-      Route::get( 'editar', [ 'user' => 'TitleDrivingAnimationController@edit' ] );
-      Route::put( 'actualiza', [ 'user' => 'TitleDrivingAnimationController@update' ] );
-      Route::delete( 'borrar', [ 'user' => 'TitleDrivingAnimationController@delete' ] );
-    } );
-    Route::group( [ 'prefix' => 'title_footer' ], function ( ) {
-      Route::get( 'editar', [ 'user' => 'TitleFooterController@edit' ] );
-      Route::put( 'actualiza', [ 'user' => 'TitleFooterController@update' ] );
-      Route::delete( 'borrar', [ 'user' => 'TitleFooterController@delete' ] );
-    } );
-    Route::group( [ 'prefix' => 'description_footer' ], function ( ) {
-      Route::get( 'editar', [ 'user' => 'DescriptionFooterController@edit' ] );
-      Route::put( 'actualiza', [ 'user' => 'DescriptionFooterController@update' ] );
-      Route::delete( 'borrar', [ 'user' => 'DescriptionFooterController@delete' ] );
-    } );
+  Route::group( [ 'prefix' => '{id}' ], function ( $id )
+  {
+    Route::resource( 'brand', 'BrandController' );
+    Route::resource( 'title-h1', 'TitleH1Controller' );
+    Route::resource( 'slider-features', 'SliderFeaturesController' );
+    Route::resource( 'title-slider-features-id', 'TitleSliderFeaturesIDController' );
+    Route::resource( 'title-gallery-fancybox', 'TitleGalleryFancyboxController' );
+    Route::resource( 'gallery-fancyboxes', 'GalleryFancyboxesController' );
+    Route::resource( 'description-gallery-fancyboxes', 'DescriptionGalleryFancyboxController' );
+    Route::resource( 'title-versions', 'TitleVersionsController' );
+    Route::resource( 'car-description-gallery-one', 'CarDescriptionGalleryOneIDController' );
+    Route::resource( 'car-description-gallery-two', 'CarDescriptionGalleryTwoIDController' );
+    Route::resource( 'title-driving-animation', 'TitleDrivingAnimationController' );
+    Route::resource( 'title-footer', 'TitleFooterController' );
+    Route::resource( 'description-footer', 'DescriptionFooterController' );
   } );
 } );
 
