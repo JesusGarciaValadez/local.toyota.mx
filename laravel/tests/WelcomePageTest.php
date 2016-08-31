61d71c1e7030bd0acffaa04a963d9c0e4bab89f3
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-08-31 17:49:29 +0000

    Make a home page and first car's model unit test

---
+++ laravel/tests/WelcomePageTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class WelcomePageTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testHomePage()
+    {
+      $this->visit( env( 'APP_URL' ) . 'highlander' )
+           ->assertResponseOk()
+           ->seePageIs( env( 'APP_URL' ) . 'highlander' )
+           ->see( 'Highlander 2016' )
+           ->see( 'Desde cualquier ángulo te parecerá espectacular.' )
+           ->see( 'Sistema de monitoreo de punto ciego.' )
+           ->see( 'CADA PARTE DE ELLA TIENE UN PROPÓSITO.' )
+           ->see( 'HIGHLANDER LE' )
+           ->see( 'CONÓCELA A FONDO Y NO BUSQUES MÁS.' )
+           ->see( 'HIGHLANDER LA CAMIONETA QUE TÚ Y TU FAMILIA ESTABAN ESPERANDO.' )
+           ->see( 'La solución ideal para los que buscan mejor rendimiento de combustible.' );
+    }
+
+    public function testCarModel()
+    {
+      $this->visit( env( 'APP_URL' ) . 'highlander' )
+           ->assertResponseOk()
+           ->seePageIs( env( 'APP_URL' ) . 'highlander' )
+           ->see( 'Ver detalle' )
+           ->click( 'Ver detalle' )
+           ->assertResponseOk()
+           ->seePageIs( env( 'APP_URL' ) . 'highlander/especificaciones/coremt' )
+           ->see( 'HIGHLANDER LE' );
+    }
+}
