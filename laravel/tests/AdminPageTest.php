0fb65123b8ee609719d85104675c68cd38ad5b4e
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-08-31 23:00:39 +0000

    Using DatabaseTransactions Trait in AdminPageTest

--- laravel/tests/AdminPageTest.php
+++ laravel/tests/AdminPageTest.php
@@ -6,74 +6,80 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class AdminPageTest extends TestCase
 {
-    /**
-     * Test if admin manager page url exists.
-     *
-     * @return void
-     */
-    public function testShowManager()
-    {
-      $this->visit( env( 'APP_URL' ) . 'admin' )
-           ->assertResponseOk();
-    }
+  use DatabaseTransactions;
 
-    /**
-     * Check if we are a guest user, send to login page
-     * @return void
-     */
-    public function testCheckIfUserIsRegistered()
-    {
-      $this->visit( env( 'APP_URL' ) . 'admin' )
-           ->assertResponseOk()
-           ->seePageIs( env( 'APP_URL' ) . 'login' );
-    }
+  /**
+   * Test if admin manager page url exists.
+   *
+   * @return void
+   */
+  public function testShowManager()
+  {
+    $this->visit( env( 'APP_URL' ) . 'admin' )
+         ->assertResponseOk();
+  }
 
-    /**
-     * Test if user registration works fine
-     * @return void
-     */
-    public function testRegisterANewUser()
-    {
-      $this->visit( env( 'APP_URL' ) . 'admin' )
-           ->assertResponseOk()
-           ->seePageIs( env( 'APP_URL' ) . 'login' )
-           ->click( 'Register' )
-           ->type( 'Jesús García', 'name' )
-           ->type( 'jesus.garcia@lunave.com', 'email' )
-           ->type( 'highlander', 'password' )
-           ->type( 'highlander', 'password_confirmation' )
-           ->press( 'Register' )
-           ->seePageIs( env( 'APP_URL' ) . 'register' )
-           ->assertResponseOk()
-           ->seeInDatabase( 'users', [
-              'name'  => 'Jesús García',
-              'email' => 'jesus.garcia@lunave.com'
-            ] );
-    }
+  /**
+   * Check if we are a guest user, send to login page
+   * @return void
+   */
+  public function testCheckIfUserIsRegistered()
+  {
+    $this->visit( env( 'APP_URL' ) . 'admin' )
+         ->assertResponseOk()
+         ->seePageIs( env( 'APP_URL' ) . 'login' );
+  }
 
-    /**
-     * Test if user is logged
-     * @return void
-     */
-    public function editCheckIfUserIsLogged()
-    {
-      $user = factory( Highlander\User::class )->create();
+  /**
+   * Test if user registration works fine
+   * @return void
+   */
+  public function testRegisterANewUser()
+  {
+    $this->visit( env( 'APP_URL' ) . 'admin' )
+         ->assertResponseOk()
+         ->seePageIs( env( 'APP_URL' ) . 'login' )
+         ->click( 'Register' )
+         ->type( 'Jesús García', 'name' )
+         ->type( 'jesus.garcia@lunave.com', 'email' )
+         ->type( 'highlander', 'password' )
+         ->type( 'highlander', 'password_confirmation' )
+         ->press( 'Register' )
+         ->seePageIs( env( 'APP_URL' ) . 'admin' )
+         ->assertResponseOk()
+         ->seeInDatabase( 'users', [
+            'name'  => 'Jesús García',
+            'email' => 'jesus.garcia@lunave.com'
+          ] );
+  }
 
-      $this->actingAs( $user )
-           ->visit( env( 'APP_URL' ) . "admin/" )
-           ->assertResponseOk( )
-           ->seePageIs( env( 'APP_URL' ) . "admin/" );
-    }
+  /**
+   * Test if user is logged
+   * @return void
+   */
+  public function editCheckIfUserIsLogged()
+  {
+    $user = factory( Highlander\User::class )->create();
 
-    public function editCheckIfUserIsLogout()
-    {
-      $user = factory( Highlander\User::class )->create();
+    $this->actingAs( $user )
+         ->visit( env( 'APP_URL' ) . "admin/" )
+         ->assertResponseOk( )
+         ->seePageIs( env( 'APP_URL' ) . "admin/" );
+  }
 
-      $this->actingAs( $user )
-           ->visit( env( 'APP_URL' ) . "admin/" )
-           ->assertResponseOk( )
-           ->seePageIs( env( 'APP_URL' ) . "admin/" )
-           ->see( 'Highlander' )
-           ->click( 'Logout' );
-    }
+  /**
+   * Test to check is user can login and logout
+   * @return void
+   */
+  public function editCheckIfUserIsLogout()
+  {
+    $user = factory( Highlander\User::class )->create();
+
+    $this->actingAs( $user )
+         ->visit( env( 'APP_URL' ) . "admin/" )
+         ->assertResponseOk( )
+         ->seePageIs( env( 'APP_URL' ) . "admin/" )
+         ->see( 'Highlander' )
+         ->click( 'Logout' );
+  }
 }
