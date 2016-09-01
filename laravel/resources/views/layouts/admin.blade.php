63b75d99f555d3ed02e80f01db0a25818e41a12a
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-09-01 19:18:22 +0000

    Create language file for spanish in admin login and register pages.

--- laravel/resources/views/layouts/admin.blade.php
+++ laravel/resources/views/layouts/admin.blade.php
@@ -47,7 +47,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="#">
-                    Laravel
+                    Toyota
                 </a>
             </div>
 
@@ -61,8 +61,8 @@
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
+                        <li><a href="{{ url('/login') }}">{{ trans( 'admin.login' ) }}</a></li>
+                        <li><a href="{{ url('/register') }}">{{ trans( 'admin.register' ) }}</a></li>
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -74,7 +74,7 @@
                                     <a href="{{ url('/logout') }}"
                                         onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
-                                        Logout
+                                        {{ trans( 'admin.logout' ) }}
                                     </a>
 
                                     <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
