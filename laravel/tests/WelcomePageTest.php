fc95f87b02b87daffccbc37e78663bce4f7deb69
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-09-15 17:37:45 +0000

    Added DatabaseTransaction trait. Delete some duplicated assertions.

--- laravel/tests/WelcomePageTest.php
+++ laravel/tests/WelcomePageTest.php
@@ -6,50 +6,50 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class WelcomePageTest extends TestCase
 {
-    /**
-     * Test if homepage is visible and contains default data.
-     *
-     * @return void
-     */
-    public function testHomePage()
-    {
-      $this->visit( env( 'APP_URL' ) . 'highlander' )
-           ->assertResponseOk()
-           ->seePageIs( env( 'APP_URL' ) . 'highlander' )
-           ->see( 'Highlander 2016' )
-           ->see( 'Desde cualquier ángulo te parecerá espectacular.' )
-           ->see( 'Sistema de monitoreo de punto ciego.' )
-           ->see( 'CADA PARTE DE ELLA TIENE UN PROPÓSITO.' )
-           ->see( 'HIGHLANDER LE' )
-           ->see( 'CONÓCELA A FONDO Y NO BUSQUES MÁS.' )
-           ->see( 'HIGHLANDER LA CAMIONETA QUE TÚ Y TU FAMILIA ESTABAN ESPERANDO.' )
-           ->see( 'La solución ideal para los que buscan mejor rendimiento de combustible.' );
-    }
+  use DatabaseTransactions;
 
-    /**
-     * Test for trying to access to the first existing car model
-     * @return void
-     */
-    public function testCarModel()
-    {
-      $this->visit( env( 'APP_URL' ) . 'highlander' )
-           ->assertResponseOk()
-           ->seePageIs( env( 'APP_URL' ) . 'highlander' )
-           ->see( 'Ver detalle' )
-           ->click( 'Ver detalle' )
-           ->assertResponseOk()
-           ->seePageIs( env( 'APP_URL' ) . 'highlander/especificaciones/coremt' )
-           ->see( 'HIGHLANDER LE' );
-    }
+  /**
+   * Test if homepage is visible and contains default data.
+   *
+   * @return void
+   */
+  public function testHomePage()
+  {
+    $this->visit( env( 'APP_URL' ) . 'highlander' )
+         ->assertResponseOk()
+         ->seePageIs( env( 'APP_URL' ) . 'highlander' )
+         ->see( 'Highlander 2016' )
+         ->see( 'Desde cualquier ángulo te parecerá espectacular.' )
+         ->see( 'Sistema de monitoreo de punto ciego.' )
+         ->see( 'CADA PARTE DE ELLA TIENE UN PROPÓSITO.' )
+         ->see( 'HIGHLANDER LE' )
+         ->see( 'CONÓCELA A FONDO Y NO BUSQUES MÁS.' )
+         ->see( 'HIGHLANDER LA CAMIONETA QUE TÚ Y TU FAMILIA ESTABAN ESPERANDO.' )
+         ->see( 'La solución ideal para los que buscan mejor rendimiento de combustible.' );
+  }
 
-    /**
-     * Test to check if a page of an invented car model exists
-     * @return void
-     */
-    public function testPageNotExist()
-    {
-      $response = $this->call( 'GET', '/especificaciones/modelo' );
+  /**
+   * Test for trying to access to the first existing car model
+   * @return void
+   */
+  public function testCarModel()
+  {
+    $this->visit( env( 'APP_URL' ) . 'highlander' )
+         ->see( 'Ver detalle' )
+         ->click( 'Ver detalle' )
+         ->assertResponseOk()
+         ->seePageIs( env( 'APP_URL' ) . 'highlander/especificaciones/coremt' )
+         ->see( 'HIGHLANDER LE' );
+  }
 
-      $this->assertEquals( 404, $response->status() );
-    }
+  /**
+   * Test to check if a page of an invented car model exists
+   * @return void
+   */
+  public function testPageNotExist()
+  {
+    $response = $this->call( 'GET', '/especificaciones/modelo' );
+
+    $this->assertEquals( 404, $response->status() );
+  }
 }
