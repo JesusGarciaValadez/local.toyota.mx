bbea40cee76f549156078a5f0af0873b72f0bd9e
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-11-28 21:05:32 +0000

    Fix js for specifications landing page

--- laravel/resources/assets/js/especificaciones.js
+++ laravel/resources/assets/js/especificaciones.js
@@ -1,7 +1,8 @@
 $( function() {
   $( '.jsn-select' ).on( 'change', function( event ) {
-    var model   = $( this ).val();
+    var model = $( event.currentTarget ).val();
+    var url   = location.hostname;
 
-    window.location.href = model;
+    window.location.href = url + '/especificaciones/' + model;
   } );
 } );
